ec1b298575a6fcb78eee05bc031be0755a497b8e
Pauline-Oraro <hiltrapauline@gmail.com>
2026-01-27 23:49:33 +0300

    homepage setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
+        <Navbar/>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import ExploreBtn from "./components/ExploreBtn"
 
 
 const page = () => {
   return (
-    <div>
-      <h1>EVENTLY</h1>
-    </div>
+    <section>
+      <h1 className="text-center">The Hub for Every Developer <br /> Event You Cannot Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreBtn/>
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+      </div>
+    </section>
   )
 }
 
